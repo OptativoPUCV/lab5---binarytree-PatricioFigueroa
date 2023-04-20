4b9240d4a8ed58266dea40cb89794b1bb1a95bba
Replit user <>
2023-04-20 22:38:49 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -159,9 +159,9 @@ Pair * upperBound(TreeMap * tree, void* key) {
         break;
       if(tree->lower_than(key,tree->current->pair->key))
       {
-        printf("%d\n", aux->pair->key);
+        printf("%p\n", aux->pair->key);
         aux = tree->current;
-        printf("%d\n", aux->pair->key);
+        printf("%p\n", aux->pair->key);
         tree->current = tree->current->left;
       }
       else tree->current = tree->current->right; 
